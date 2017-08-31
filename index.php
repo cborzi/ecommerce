2b3ce61eebdf5fd08e8efbc111325c0ed8b3de95
cborzi <carlosborzi@yahoo.com.br>
2017-08-31 14:56:56 -0300

    Restaucao maquina 31/08/2017

--- index.php
+++ index.php
@@ -92,15 +92,32 @@ $app->get('/admin/users/:iduser', function($iduser) {
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get(int()$iduser);
+
 	$page = new PageAdmin();
 
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
 
 });
 
-$app->post('/admin/create', function() {
+$app->post('/admin/users/create', function() {
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
@@ -112,4 +129,4 @@ $app->post('/admin/users/:iduser', function($iduser) {
 
 $app->run();
 
- ?>
+?>
